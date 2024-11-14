d76646b79e8f782a191c258441012d7ccee509bb
aryndraa <aryacoder1102@gmail.com>
2024-11-14 10:44:15 +0800

    remake landing page

---
+++ src/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("details").forEach((detail) => {
+    detail.addEventListener("toggle", function () {
+      if (detail.open) {
+        document.querySelectorAll("details").forEach((otherDetail) => {
+          if (otherDetail !== detail && otherDetail.open) {
+            otherDetail.open = false;
+          }
+        });
+      }
+    });
+  });
